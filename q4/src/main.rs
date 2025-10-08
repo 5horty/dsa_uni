8d63d27879f81bc79ff83c23c4cfb33e5817183b
5horty <roganemacleod@gmail.com>
2025-10-08 21:24:52 +0100

    last empty attempt

--- q4/src/main.rs
+++ q4/src/main.rs
@@ -14,21 +14,25 @@ impl circle{
         }
     }
     fn add(&mut self, val: i32){
-        if self.back < 1024{
-            self.val[self.back] = val; 
-            self.back += 1;
-        }else{
-            self.back = (self.back+1) % 1024;
+        let next = (self.back+1) % 1024;
+        if next == self.front{
+            return;
         }
+        self.val[self.back] = val; 
+        self.back = next;
     }
     fn removed(&mut self){
-        if self.front <= 1024{
-            self.val[self.front] = 0;
-            self.front += 1;
-
-        }else{
-            self.front = (self.front+1) % 1024;
+        if self.front == self.back{
+            return;
         }
+        self.val[self.front] = 0;
+        self.front = (self.front+1) % 1024;
+    }
+    fn is_empty(&self)->bool{
+        self.front == self.back
+    }
+    fn is_full(&self)-> bool{
+        (self.back + 1)% 1024 == self.front  
     }
 }
 
@@ -41,14 +45,15 @@ impl circle{
 
 fn main() {
     let mut a = circle::new();
-    println!("{a:?}");
-    for i in 0..1025{
+    for i in 0..=1025{
 
         a.add(5);
     }
+    println!("{}",a.is_full()); // true
     println!("{a:?}");
-    for i in 0..1024{
+    for i in 0..=1024{
         a.removed();
     }
+    println!("{}",a.is_empty()); // true
     println!("{a:?}");
 }
